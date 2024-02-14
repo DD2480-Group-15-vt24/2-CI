d4368d97fb74d7554d7f7fc862f3aba03e16fc37
SihamShahoud <113520260+SihamShahoud@users.noreply.github.com>
2024-02-14 09:34:50 +0100

    Fix: Comments on code i database

--- src/db.py
+++ src/db.py
@@ -3,6 +3,9 @@ import sqlite3
 
 
 def create_database():
+    """
+    Create database db.py 
+    """
     conn = sqlite3.connect("ci.db")
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY,commit_hash TEXT, date TEXT, logs TEXT)")
@@ -10,8 +13,10 @@ def create_database():
     conn.close()
 
 
-# Function to add row of history to the SQLite database
 def insert_row(commit_hash, logs):
+    """
+    Function to add row of history to the SQLite database
+    """
     conn = sqlite3.connect("ci.db")
     cursor = conn.cursor()
     date = datetime.now().strftime("%Y-%M-%D %H:%M:%S")
@@ -21,16 +26,14 @@ def insert_row(commit_hash, logs):
     conn.commit()
     conn.close()
     
-# Function to get all rows from SQLite database table
-    def get_all_rows():
-        conn = sqlite3.connect("ci.db")
-        cursor = conn.cursor()
-        cursor.execute("SELECT * history")
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
-        
-        
-    
-if __name__ == "__main__":
-    create_database()
+
+def get_all_rows():
+    """
+    Function to get all rows from SQLite database table
+    """
+    conn = sqlite3.connect("ci.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * history")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
